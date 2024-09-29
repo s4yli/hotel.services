54e480e32300d5524a84e1823c1ce924721c9237
EL YAAGOUBI Ilyas <ilyas.elyaagoubi@hotmail.com>
2024-09-29 19:03:02 +0200

    Initial commit

---
+++ src/test/java/com/isima/hotel_services/UnitTests.java
@@ -0,0 +1,13 @@
+package com.isima.hotel_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
